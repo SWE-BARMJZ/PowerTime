ea5fd997886e00d8297005a223403f82fd30b22c
Zyad Samy <zyadsamysobky@gmail.com>
2022-11-28 18:14:28 +0200

    setup backend project

---
+++ backend/src/main/java/com/barmjz/productivityapp/ProductivityAppApplication.java
@@ -0,0 +1,13 @@
+package com.barmjz.productivityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityAppApplication.class, args);
+	}
+
+}
