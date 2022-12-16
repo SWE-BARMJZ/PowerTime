11e0cae0fc95f5d7781be17a18f3338c08a7c467
ZyadSamy <67867551+ZyadSamy@users.noreply.github.com>
2022-12-16 22:12:30 +0200

    Fix duplicate first name on Signup page

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { registerUser } from "../api/user.api";
+
 import {
   Button,
   Text,
@@ -13,10 +15,8 @@ import {
   Icon,
   useToast,
 } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
-
 import FormInput from "../UI/FormInput";
-import { registerUser } from "../api/user.api";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const logoPath = require("../assets/images/LOGO.png");
 
@@ -84,7 +84,7 @@ export const SignUp = ({ navigation }) => {
 
             <FormInput label="Email" onChange={emailChangeHandler} />
             <FormInput label="First name" onChange={firstNameChangeHandler} />
-            <FormInput label="First name" onChange={lastNameChangeHandler} />
+            <FormInput label="Last name" onChange={lastNameChangeHandler} />
             <FormInput
               label="Password"
               onChange={passwordChangeHandler}
@@ -138,119 +138,6 @@ export const SignUp = ({ navigation }) => {
         </Hidden>
       </HStack>
     </Box>
-    // <View style={Styles.device}>
-    //   <View style={Styles.container}>
-    //     <Image source={logoPath} style={Styles.logo} />
-
-    //     <Title>Sign Up</Title>
-    //     <Field name={"Email"} onChangeText={emailChangeHandler} />
-    //     <Field name={"First Name"} onChangeText={firstNameChangeHandler} />
-    //     <Field name={"Last Name"} onChangeText={lastNameChangeHandler} />
-    //     <Field
-    //       name={"Password"}
-    //       onChangeText={passwordChangeHandler}
-    //       secureTextEntry={true}
-    //     />
-    //     <Pressable style={Styles.button} onPress={signupHandler}>
-    //       <Text style={Styles.buttonText}>Sign Up</Text>
-    //     </Pressable>
-    //     <View style={Styles.lastLine}>
-    //       <Text style={{ fontSize: 16 }}>
-    //         Already have an account?
-    //         <View style={Styles.inLineLink}>
-    //           <Pressable onPress={goToLogin}>
-    //             <Text style={Styles.mediumLink}>Log in</Text>
-    //           </Pressable>
-    //         </View>
-    //       </Text>
-    //     </View>
-    //   </View>
-    //   {Platform.OS == "web" && (
-    //     <View style={Styles.imgContainer}>
-    //       <Image
-    //         source={require("../assets/images/themeImage.png")}
-    //         style={Styles.themeImage}
-    //       />
-    //     </View>
-    //   )}
-    // </View>
   );
 };
 
-// const Styles = StyleSheet.create({
-//     device: {
-//       flexDirection: "row",
-//       backgroundColor: "#dffaef",
-//       alignItems: "flex-start",
-//       flex: 1
-//     },
-
-//     container: {
-//       marginHorizontal: "5%",
-//       flex: 2,
-//       justifyContent: "flex-start",
-//       backgroundColor: "#dffaef",
-//       height: "100%",
-//     },
-
-//     logo: {
-//       height: "15%",
-//       width: "54%",
-//       resizeMode: "contain",
-//     },
-
-//     imgContainer: {
-//       marginHorizontal: "5%",
-//       flex: 4,
-//       justifyContent: "center",
-//       backgroundColor: "#dffaef",
-//       height: "100%",
-//     },
-
-//     themeImage: {
-//       resizeMode: "contain",
-//       height: "100%",
-//       borderRadius: 38,
-//     },
-
-//     button: {
-//       marginVertical: "5%",
-//       backgroundColor: "#0066F9",
-//       borderRadius: 20,
-//       alignItems: "center",
-//       justifyContent: "center",
-//       height: "8%",
-//     },
-
-//     buttonText: {
-//       color: "white",
-//       fontSize: 20,
-//       fontWeight: "bold",
-//     },
-
-//     farLink: {
-//       marginVertical: "4%",
-//       marginLeft: "5%",
-//       width: "50%",
-//     },
-
-//     lastLine: {
-//       marginVertical: "4%",
-//       justifyContent: "center",
-//       alignItems: "center",
-//     },
-
-//     smallLink: {
-//       fontWeight: "bold",
-//       textDecorationLine: "underline",
-//       fontSize: 14,
-//     },
-
-//     inLineLink: {},
-
-//     mediumLink: {
-//       fontWeight: "bold",
-//       textDecorationLine: "underline",
-//       fontSize: 16,
-//     },
-//   });
