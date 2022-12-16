34b5c71f9086e54d1e1d60dfc6597901585405b0
Zyad Samy <zyadsamysobky@gmail.com>
2022-12-16 20:59:29 +0200

    update login validation

--- frontend/api/auth.api.js
+++ frontend/api/auth.api.js
@@ -1,7 +1,6 @@
 import { BACKEND_URL } from "./const";
 
 export const requestToken = async ({ email, password }) => {
-  console.log(email, password);
   const url = `${BACKEND_URL}/api/token`;
   const str = `${email}:${password}`;
   const auth = `Basic ${Buffer.from(str).toString("base64")}`;
@@ -9,10 +8,11 @@ export const requestToken = async ({ email, password }) => {
   const response = await fetch(url, {
     method: "POST",
     headers: new Headers({ Authorization: auth }),
+  }).catch((error) => {
+    throw new Error("Problem connecting with the server!");
   });
 
-  if (response.status !== 200) {
-    throw new Error(response.status);
-  } 
-  return response;
+  if (response.status === 200) return response;
+  if (response.status === 401) throw new Error("Invalid credentials");
+  else throw new Error();
 };
