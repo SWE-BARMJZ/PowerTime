7bb912929e3533734a1fbfa0460651c1af13f014
BaselAhmed <basel20ahmed@gmail.com>
2022-12-26 22:10:03 +0200

    created pomodoro component

---
+++ frontend/components/Pomodoro.js
@@ -0,0 +1,23 @@
+import React, { useContext, useState } from "react";
+import {
+  Button,
+  Text,
+  Image,
+  Heading,
+  HStack,
+  Link,
+  Box,
+  VStack,
+  Hidden,
+  useToast,
+} from "native-base";
+
+const logoPath = require("../assets/images/LOGO.png");
+
+export const Pomodoro = ({ navigation }) => {
+    return (
+        <Text>Pomodoro Page</Text>
+    )
+};
+
+
