f8e2126b0c1ddcd6cf35d76e2f126df31817bb3c
NoirFLamme <ramyfetteha@gmail.com>
2022-12-30 20:48:31 +0200

    added tasks

--- frontend/api/task.api.js
+++ frontend/api/task.api.js
@@ -1,12 +1,120 @@
-const fetchTask = (id, token) => {};
-const updateTask = (id, updates, token) => {};
-const deleteTask = (id, token) => {};
-const createTask = (task, token) => {};
-const tickTask = (id, token) => {};
-const untickTask = (id, token) => {};
+import { async } from "q";
+import { BACKEND_URL } from "./const";
 
-const fetchTasks = (token) => {};
-const fetchCompletedTasks = (token) => {};
+
+const fetchTask = async (id, token) => {
+    const url = `${BACKEND_URL}/api/task/${id}`;
+  
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.json();
+};
+const updateTask = async (id, updates, token) => {
+    const url = `${BACKEND_URL}/api/task/${id}`;
+  
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: { 
+        Authorization: `Bearer ${token}`, 
+        "Content-type": "application/json" },
+      body: JSON.stringify(updates),
+    });
+  
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.json();
+};
+const deleteTask = async (id, token) => {
+    const url = `${BACKEND_URL}/api/task/${id}`;
+  
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.text();
+
+};
+const createTask = async (task, token) => {
+  const url = `${BACKEND_URL}/api/task/taskType=${task}`;
+  
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { 
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json" },
+      body: JSON.stringify(task),
+    });
+  
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.json();
+};
+const tickTask = async (id, token, date, taskType) => {
+  const url = `${BACKEND_URL}/api/task/${id}/tick?date=${date}&taskType=${taskType}`;
+  
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.json();
+
+};
+const untickTask = async (id, token, date) => {
+  const url = `${BACKEND_URL}/api/task/${id}/untick?date=${date}`;
+  
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (response.status !== 200) {
+    throw new Error(response.status);
+  }
+  return response.json();
+};
+
+const fetchTasks = async (token) => {
+  const url = `${BACKEND_URL}/api/task/`;
+  
+  const response = await fetch(url, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (response.status !== 200) {
+    throw new Error(response.status);
+  }
+  return response.json();
+};
+const fetchCompletedTasks = async (token) => {
+  const url = `${BACKEND_URL}/api/task/completed`;
+  
+  const response = await fetch(url, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (response.status !== 200) {
+    throw new Error(response.status);
+  }
+  return response.json();
+};
 
 export const TASK_API = {
   fetchTask,
