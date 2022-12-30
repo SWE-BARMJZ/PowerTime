4a25f39fba33d1221440d4265478470a41c2cc04
Ali <hsnalshrawyly56@gmail.com>
2022-12-30 07:36:51 +0200

    PA-72 <initial pomodoro DB tests>

--- backend/src/test/java/com/barmjz/productivityapp/Pomodoro/UserPomoDB.java
+++ backend/src/test/java/com/barmjz/productivityapp/Pomodoro/UserPomoDB.java
@@ -2,6 +2,7 @@ package com.barmjz.productivityapp.Pomodoro;
 
 import com.barmjz.productivityapp.pomodoro.Pomodoro;
 import com.barmjz.productivityapp.pomodoro.PomodoroRepo;
+import com.barmjz.productivityapp.pomodoro.PomodoroSession;
 import com.barmjz.productivityapp.user.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,7 @@ public class UserPomoDB {
     @Autowired PomodoroRepo pomodoroRepo;
     User user1;
     Pomodoro pomodoro1;
+    PomodoroSession pomodoroSession;
 
     @BeforeEach
     void setUp(){
@@ -32,12 +34,31 @@ public class UserPomoDB {
                 .breakTime(5)
                 .studyTime(25)
                 .build();
+        pomodoroSession = PomodoroSession.builder()
+                .user(user1)
+                .isStudying(true)
+                .startTime(5L)
+                .build();
         pomodoroRepo.save(pomodoro1);
     }
 
     @Test
     void checkUserExist(){
         assertThat(pomodoroRepo.existsPomodoroByUserId(user1.getId())).isTrue();
+        assertThat(pomodoroRepo.getPomodoroByUserId(user1.getId()).getUser()).isEqualTo(user1);
+        assertThat(pomodoroRepo.getPomodoroByUserId(user1.getId()).getUser().getId()).isEqualTo(user1.getId());
+        assertThat(pomodoroRepo.getPomodoroByUserId(user1.getId()).getUser().getEmail()).isEqualTo(user1.getEmail());
+    }
+
+    @Test
+    void checkPomodoroExist(){
+        assertThat(pomodoroRepo.getPomodoroByUserId(pomodoro1.getId())).isEqualTo(pomodoro1);
+    }
+
+    @Test
+    void checkPomodoroInstance(){
+        assertThat(pomodoroRepo.getPomodoroByUserId(pomodoro1.getId())).isInstanceOf(Pomodoro.class);
+        assertThat(pomodoroRepo.getPomodoroByUserId(pomodoro1.getId())).isNotInstanceOf(PomodoroSession.class);
     }
 
 
